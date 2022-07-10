7ad459b9dc777af07a8b3dd8f1e934fbbc31ab53
AmX <amx281@gmail.com>
2022-07-10 02:09:01 +0200

    Hypotenuse exercise

---
+++ Practice/hypotenuse.js
@@ -0,0 +1,26 @@
+let a;
+let b;
+let c;
+
+// a = window.prompt("Unesite vrednost za a :");
+// a = Number(a);
+
+// b = window.prompt("Unesite vrednost za b :");
+// b = Number(b);
+
+// c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 3));
+
+// console.log("Vrednost hipotenuze B je:", c);
+
+document.getElementById("potvrdiD").onclick = function () {
+  a = document.getElementById("boxA").value;
+  a = Number(a);
+
+  b = document.getElementById("boxB").value;
+  b = Number(b);
+
+  c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+  document.getElementById("vrednostC").innerHTML =
+    "Vrednost hipotenuze B je:" + c;
+};
