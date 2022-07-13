df58dea15095770a8038fb4108961dc0161717d5
AmX <amx281@gmail.com>
2022-07-13 23:56:52 +0200

    hyp

--- Practice/hypotenuse.js
+++ Practice/hypotenuse.js
@@ -4,12 +4,9 @@ let c;
 
 // a = window.prompt("Unesite vrednost za a :");
 // a = Number(a);
-
 // b = window.prompt("Unesite vrednost za b :");
 // b = Number(b);
-
 // c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 3));
-
 // console.log("Vrednost hipotenuze B je:", c);
 
 document.getElementById("potvrdiD").onclick = function () {
